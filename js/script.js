590c0c83bd8a5fff9224347a703a8a4f5009f7df
BritishPandaChick <sbartley1080@gmail.com>
2021-12-16 15:03:58 -0500

    Remove hide class in index.html file. Add change event and adjust async function

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 const randomFolks = document.querySelector(".random-peeps");
+const selectUserNumber = document.querySelector("#users");
 
-const getData = async function () {
-    const usersRequest = await fetch("https://randomuser.me/api?results=5");
-    const data = await userRequest.json();
+const getData = async function (numUsers) {
+    const usersRequest = await fetch(`https://randomuser.me/api?results=${numUsers}`);
+    const data = await usersRequest.json();
     //console.log(data);
+
     const userResults = data.results;
     //console.log(userResults);
     displayUsers(userResults);
 }
+
+getData(1);
 
 const displayUsers = function (userResults) {
     randomFolks.innerHTML = "";
@@ -25,3 +29,8 @@ const displayUsers = function (userResults) {
         randomFolks.append(userDiv)
     }
 }
+
+selectUserNumber.addEventListener("change", function(e) {
+    const numUsers = e.target.value;
+    getData(numUsers);
+});
